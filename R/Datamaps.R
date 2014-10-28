fec4f590549c009d60af7458e1a100d50cad3fd2
akzaidi <alikaz.zaidi@gmail.com>
2014-10-28 13:03:16 -0400

    Update Datamaps.R

--- R/Datamaps.R
+++ R/Datamaps.R
@@ -40,7 +40,7 @@ processChoroData <- function(x, data, pal, map = 'usa', ...){
 #' 
 #' 
 #' 
-ichoropleth <- function(x, data, pal = "Blues", ncuts = 5, animate = NULL, play = F, map = 'usa', legend = TRUE, labels = TRUE, man.labels = NULL, ...){
+ichoropleth <- function(x, data, pal = "Blues", ncuts = 5, animate = NULL, play = F, map = 'usa', legend = TRUE, labels = TRUE, ...){
   d <- Datamaps$new()
   fml = lattice::latticeParseFormula(x, data = data)
   data = transform(data, 
@@ -51,11 +51,10 @@ ichoropleth <- function(x, data, pal = "Blues", ncuts = 5, animate = NULL, play 
       include.lowest = TRUE
     )
   )
-  fill.labels <- ifelse(length(man.labels) > 0, man.labels, levels(data$fillKey))
   fillColors = brewer.pal(ncuts, pal)
   d$set(
     scope = map, 
-    fills = as.list(setNames(fillColors, fill.labels)), 
+    fills = as.list(setNames(fillColors, levels(data$fillKey))),
     legend = legend,
     labels = labels,
     ...
